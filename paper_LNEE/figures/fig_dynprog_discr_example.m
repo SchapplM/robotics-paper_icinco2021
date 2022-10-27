06131bfd7f217cd44ba3c7416d8361d42039c0ca
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2022-10-27 09:09:28 +0200

    Wahl des neuen Modus "stageopt" hier. Angleichung an Intervall-Skript

--- paper_LNEE/figures/fig_dynprog_discr_example.m
+++ paper_LNEE/figures/fig_dynprog_discr_example.m
@@ -11,7 +11,8 @@
 clc
 clear
 % User settings
-usr_highres_distrfig = true; % high resolution of the paper figure for performance criterion map
+usr_highres_distrfig = false; % high resolution of the paper figure for performance criterion map
+usr_stageoptmode = true;
 %% Initialize
 % Initialize Robot
 % Default output directory (for paper)
@@ -35,16 +36,31 @@ XL = d.XL;
 phiz_range = d.phiz_range;
 s_ref = d.s_ref;
 s_tref = d.s_tref;
-filename_dynprog= fullfile(data_path, [filename_pre, '_dynprog_costRMStraj_n9_', 'nored', '', '.mat']);
+if ~usr_stageoptmode
+  filename_post = ['_dynprog_costRMStraj_n9_', 'nored', '', '.mat'];
+else
+  filename_post = ['_dynprog_costRMStraj_n9_', 'nored', '_stageopt', '.mat'];
+end
+filename_dynprog= fullfile(data_path, [filename_pre, filename_post]);
 assert(exist(filename_dynprog, 'file'), 'dynamic programming results file does not exist');
 d = load(filename_dynprog);
 DP_XE = d.DP_XE;
 DP_Stats = d.DP_Stats;
 DP_TrajDetail = d.DP_TrajDetail;
 DP_settings = d.DP_settings;
-dpres_dir = fullfile(this_dir, '..', '..', 'case_study', 'LNEE_Traj1_DP_debug_costRMStraj_n9_nored');
+stageoptstr = '';
+if ~usr_stageoptmode 
+  debugfoldername_full = 'LNEE_Traj1_DP_debug_costRMStraj_n9_nored';
+else
+  debugfoldername_full = 'LNEE_Traj1_DP_debug_costRMStraj_n9_nored_stageopt';
+  stageoptstr= '_stageopt';
+end
+dpres_dir = fullfile(this_dir, '..', '..', 'case_study', debugfoldername_full);
 assert(exist(dpres_dir, 'file'), 'directory with debug information for DP does not exist');
 
+% Eigenschaften der Intervalle aus Ergebnis ausgeben lassen. Siehe dynprog_taskred_ik
+delta_phi = DP_Stats.delta_phi;
+phi_range = DP_Stats.phi_range;
 %% Prepare performance map plot
 % Umrechnung auf hnpos
 abort_thresh_hpos = NaN(RP.idx_ik_length.hnpos, 1);
@@ -60,7 +76,7 @@ settings_perfmap = struct( ...
   'extend_map', false, ... % sowieso nicht sichtbar
   'wn', DP_settings.wn, ...
   'abort_thresh_h', abort_thresh_hpos);
-d_final = load(fullfile(dpres_dir, 'dp_final.mat'), 'I_all');
+d_final = load(fullfile(dpres_dir, 'dp_final.mat'), 'I_all', 'I_best');
 %% Plot performance map with three first stages (Figure 3 of the paper)
 fprintf('Zeichne Bilder für DP Einzeltransfer\n');
 pmfhdl = change_current_figure(1); clf;
@@ -80,6 +96,7 @@ for i_stage1 = 1:3
   d_state = load(fullfile(dpres_dir, sprintf('dp_stage%d_final.mat', i_stage1)));
   tfn = dir(fullfile(dpres_dir, sprintf('dp_stage%d_state*_to*_result.mat', i_stage1)));
   % Gehe alle Transfers durch, die gespeichert sind
+  hdl_all = NaN(length(tfn), 2); % Erste Spalte Handle, zweite Spalte Kategorie als Zahl 
   for ii = 1:length(tfn)
     d_ii = load(fullfile(dpres_dir, tfn(ii).name));
     i_state1 = d_ii.k;
@@ -89,44 +106,62 @@ for i_stage1 = 1:3
     Iplot = select_plot_indices_downsample_nonuniform(...
       s_stage(1:length(d_ii.X6_traj)), d_ii.X6_traj, 0.05, 3*pi/180);
 %     hdl = plot(s_stage(1:length(d_i.X6_traj)), 180/pi*d_i.X6_traj, 'k--'); % TODO: _ref
-    hdl = plot(s_stage(Iplot), 180/pi*d_ii.X6_traj(Iplot), 'k-');
-    set(hdl, 'LineWidth', 1);
+    hdl_all(ii,1) = plot(s_stage(Iplot), 180/pi*d_ii.X6_traj(Iplot), 'k-');
+    set(hdl_all(ii,1), 'LineWidth', 1);
+    if usr_stageoptmode && i_state2 > size(d_final.I_all,2)
+      set(hdl_all(ii,1), 'LineStyle', '--'); % Teil der neu optimierten Linien
+    end
     % Prüfe ob es sich um die optimale Teil-Politik handelt
-    if d_final.I_all(i_stage1+1,i_state2) == i_state1
-      set(hdl, 'Color', 'm');
-      DP_hdl(3) = hdl; % optimal transmission (to this stage)
+    if i_state2 <= size(d_final.I_all,2) && d_final.I_all(i_stage1+1,i_state2) == i_state1
+      hdl_all(ii,2) = 1;
+      set(hdl_all(ii,1), 'Color', 'm');
+      DP_hdl(3) = hdl_all(ii,1); % optimal transmission (to this stage)
     elseif ~isinf(d_state.F_stage(i_state1, i_state2))
-      set(hdl, 'Color', 'c');
-      DP_hdl(2) = hdl; % line for valid transition
+      hdl_all(ii,2) = 2;
+      set(hdl_all(ii,1), 'Color', 'c');
+      DP_hdl(2) = hdl_all(ii,1); % line for valid transition
     else
-      DP_hdl(1) = hdl; % line for invalid transition
-      % Setze Linie ganz nach unten (damit i.O. immer oben sind)
-      ZOrderSet(hdl, 0);
-      % Farbkarte muss danach wieder nach unten gesetzt werden
-      ZOrderSet(Hdl_all.surf, 0);
+      hdl_all(ii,2) = 3;
+      if i_state2 <= size(d_final.I_all,2)
+        DP_hdl(1) = hdl_all(ii,1); % line for invalid transition
+      else
+        DP_hdl(4) = hdl_all(ii,1); % for stage-optimization legend entry
+      end
     end
   end
   if isnan(DP_hdl(2))
     % Es gibt keine "valid"-Linie, da alle Lösungen stage-optimal sind
     DP_hdl(2) = plot(NaN, NaN, 'c-', 'LineWidth', 1);
   end
+  % Gehe alle Handles durch. Zuerst alle cyan nach hinten, dann alle
+  % schwarzen, dann Farbkarte
+  for ii = find(hdl_all(:,2)==2)' % 2=cyan
+    ZOrderSet(hdl_all(ii,1), 0); % Setze Linie ganz nach unten (damit optimale immer oben sind)
+  end
+  for ii = find(hdl_all(:,2)==3)' % 3=black
+    ZOrderSet(hdl_all(ii,1), 0); % Setze Linie ganz nach unten (damit i.O. immer oben sind)
+  end
+  % Farbkarte muss danach wieder nach unten gesetzt werden
+  ZOrderSet(Hdl_all.surf, 0);
   xlim(minmax2(s_stage'));
   ylim([-70, 130]);
-  if i_stage1 ~= 2
-    xlabel('');
-  else
-    xlabel('Normalized trajectory progress $s$', 'interpreter', 'latex');
-  end
+
+  % Put text for state interval
   if i_stage1 > 1
     ylabel('');
   else
     ylabel('Redundant coordinate $\varphi_z$ in deg', 'interpreter', 'latex');
   end
-  if i_stage1 < 3
+  if i_stage1 < length(axhdl)
     delete(colorbar());
   end
   title(sprintf('\\textbf{(%s)} stage %d', char(96+i_stage1), i_stage1), ...
     'interpreter', 'latex');
+  if i_stage1 ~= 2
+    xlabel('');
+  else
+    xlabel('Normalized trajectory progress $s$', 'interpreter', 'latex');
+  end
 end
 set(Hdl_all.cb, 'Position', [0.90, 0.03, 0.02, 0.95]); % put cb to the right
 cyh=ylabel(Hdl_all.cb, 'Performance criterion $h$ (cond.)', 'Rotation', 90, 'interpreter', 'latex');
@@ -141,8 +176,13 @@ set_size_plot_subplot(pmfhdl, ...
 drawnow();
 % Legende
 I_vmactive = [2 4]; % Manuelle Auswahl der aktiven Marker. Referenz: s_pmp.violation_markers
-LegHdl = [DP_hdl; VM_hdl(I_vmactive)];
-LegLbl = ['invalid', 'valid', 'optimal', s_pmp.violation_markers(1,I_vmactive)];
+if usr_stageoptmode == 0
+  LegHdl = [DP_hdl(1:3); VM_hdl(I_vmactive)];
+  LegLbl = ['invalid', 'valid', 'optimal', s_pmp.violation_markers(1,I_vmactive)];
+else
+  LegHdl = [DP_hdl; VM_hdl(I_vmactive)];
+  LegLbl = ['invalid', 'valid', 'optimal', 'add. stage opt.', s_pmp.violation_markers(1,I_vmactive)];
+end
 % LegLbl{strcmp(LegLbl,'qlim_hyp')} = 'Joint Limit';
 LegLbl{strcmp(LegLbl,'jac_cond')} = 'singularity';
 LegLbl{strcmp(LegLbl,'coll_hyp')} = 'collision';
@@ -155,6 +195,7 @@ legendflex(LegHdl, LegLbl, 'anchor', {'n','n'}, ...
   ... 'padding', [-3,-3,3], ... % Leerraum reduzieren
   'box', 'on');
 pdfname = 'dp_discr_stage1_to_3';
+
 exportgraphics(pmfhdl, fullfile(paperfig_path, [pdfname, '.pdf']),'ContentType','vector') 
 
 % Zur Kompression des Bildes (bringt ca. 30% bei hoher Auflösung):
@@ -166,7 +207,7 @@ ghostscript(['-dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 ', ..
 fprintf('Zeichne gesamte Redundanzkarte für DP\n');
 settings_perfmap_complete = settings_perfmap;
 settings_perfmap_complete.markermindist = [0.2, 15]; % Größerer Markerabstand, da kleiner im Bild
-pmfhdl = change_current_figure(1); clf; hold on;
+pmfhdl = change_current_figure(3); clf; hold on;
 DP_hdl = NaN(3,1); % Handle für die verschiedenen Linien (für Legende)
 t1 = tic();
 [Hdl_all, s_pmp] = RP.perfmap_plot(H_all, phiz_range, s_ref, settings_perfmap_complete);
@@ -191,7 +232,12 @@ for i_stage1 = 1:length(DP_settings.IE)-1
     Iplot = select_plot_indices_downsample_nonuniform(...
       s_stage(1:length(d_ii.X6_traj)), d_ii.X6_traj, 0.05, 3*pi/180);
     hdl = plot(s_stage(Iplot), 180/pi*d_ii.X6_traj(Iplot), 'c-', 'LineWidth', 1);
-    if d_final.I_all(i_stage1+1,i_state2) == i_state1
+    if d_final.I_best(i_stage1) == i_state1 && ...
+        d_final.I_best(i_stage1+1) == i_state2
+      set(hdl, 'Color', 'b');
+      set(hdl, 'LineWidth', 1.5);
+      DP_hdl(3) = hdl; % optimal transmission (globally)
+    elseif i_state2 <= size(d_final.I_all,2) &&  d_final.I_all(i_stage1+1,i_state2) == i_state1
       DP_hdl(2) = hdl; % optimal transmission (to this stage)
       set(DP_hdl(2), 'Color', 'm');
     else
@@ -199,10 +245,10 @@ for i_stage1 = 1:length(DP_settings.IE)-1
     end
   end
 end
-d_output = load(fullfile(dpres_dir, 'dp_output.mat'), 'TrajDetail');
-Iplot = select_plot_indices_downsample_nonuniform(...
-  DP_settings.PM_s_tref, d_output.TrajDetail.X6, 0.05, 3*pi/180);
-DP_hdl(3) = plot(DP_settings.PM_s_tref, 180/pi*d_output.TrajDetail.X6, 'b-', 'LineWidth', 2);
+% d_output = load(fullfile(dpres_dir, 'dp_output.mat'), 'TrajDetail');
+% Iplot = select_plot_indices_downsample_nonuniform(...
+%   DP_settings.PM_s_tref, d_output.TrajDetail.X6, 0.05, 3*pi/180);
+% DP_hdl(3) = plot(DP_settings.PM_s_tref, 180/pi*d_output.TrajDetail.X6, 'b-', 'LineWidth', 2);
 xlabel('Normalized trajectory progress $s$', 'interpreter', 'latex');
 ylabel('Redundant coordinate $\varphi_z$ in deg', 'interpreter', 'latex');
 xlim(minmax2(DP_settings.PM_s_tref'));
@@ -232,7 +278,9 @@ legendflex(LegHdl, LegLbl, 'anchor', {'n','n'}, ...
   'padding', [1,1,0], ... % Leerraum reduzieren
   'box', 'on');
 pdfname = 'dp_discr_result';
+% TODO: Beide Export-Befehle führen bei Windows-Laptop zu Fehler.
 exportgraphics(pmfhdl, fullfile(paperfig_path, [pdfname, '.pdf']),'ContentType','vector') % ,'Resolution','100'
+export_fig(pmfhdl, fullfile(paperfig_path, [pdfname, '2.pdf']));
 cd(paperfig_path);
 ghostscript(['-dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 ', ...
   '-dPDFSETTINGS=/prepress -sOutputFile=',pdfname,'_compressed.pdf ',pdfname,'.pdf']);
